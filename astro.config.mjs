4de29a4ed68349941c3829c2690b1064477832db
Max Niederman <max@maxniederman.com>
2021-07-25 13:08:41 -0700

    style: fix config formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,11 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: 'http://maxniederman.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
+    site: "http://maxniederman.com", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true, // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
     tailwindConfig: "./tailwind.config.js", // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
   },
-  renderers: [
-    "@astrojs/renderer-react",
-    "@astrojs/renderer-svelte",
-  ],
+  renderers: ["@astrojs/renderer-react", "@astrojs/renderer-svelte"],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,30 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-	purge: ["public/**/*.html", "src/**/*.{astro,tsx,svelte}"],
-	darkMode: "media",
-	theme: {
-		colors: {
-			transparent: "transparent",
-			current: "currentColor",
-			black: "black",
-			white: "white",
-		},
-		extend: {
+  purge: ["public/**/*.html", "src/**/*.{astro,tsx,svelte}"],
+  darkMode: "media",
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "black",
+      white: "white",
+    },
+    extend: {
       minHeight: {
-        "screen": "100vh",
+        screen: "100vh",
       },
       minWidth: {
-        "screen": "100vw",
+        screen: "100vw",
       },
-			fontFamily: {
-				serif: ["EB Garamond", ...fontFamily.serif],
+      fontFamily: {
+        serif: ["EB Garamond", ...fontFamily.serif],
         mono: ["Fira Code", ...fontFamily.mono],
-			},
-			fontSize: {
-				"10xl": "12rem",
-				"11xl": "16rem",
-			},
+      },
+      fontSize: {
+        "10xl": "12rem",
+        "11xl": "16rem",
+      },
     },
-  },	
+  },
 };
