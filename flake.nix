674bc94d4f34e5687e56d052a25dcb01aedcff04
Max Niederman <max@maxniederman.com>
2022-01-08 20:28:07 -0800

    build(flake): add Nix Flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            # dependencies
+            nodePackages.pnpm
+          ];
+        };
+      }
+    );
+}
