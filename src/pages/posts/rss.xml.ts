3494a8f8d007c99c8fcdf16897c74a53cd1904e1
Max Niederman <max@maxniederman.com>
2022-07-07 17:47:37 -0700

    feat(rss): add content to rss feed

--- src/pages/posts/rss.xml.ts
+++ src/pages/posts/rss.xml.ts
@@ -4,21 +4,15 @@ const posts = Object.values(import.meta.globEager("./**/*.md"));
 
 export const get = () =>
   rss({
-    // `<title>` field in output xml
     title: "Max Niederman’s Blog",
-    // `<description>` field in output xml
     description: "Essays on programming, math, and more.",
-    // base URL for RSS <item> links
-    // SITE will use "site" from your project's astro.config.
     site: import.meta.env.SITE,
-    // list of `<item>`s in output xml
-    // simple example: generate items for every md file in /src/pages
-    // see "Generating items" section for required frontmatter and advanced use cases
+    stylesheet: "/rss/styles.xsl",
     items: posts.map((post) => ({
       link: post.url,
       title: post.frontmatter.title,
       pubDate: post.frontmatter.published,
+      description: post.compiledContent(),
     })),
-    // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
