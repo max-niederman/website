6973b4d7bca32206f6efcab077acd31cbc513739
Max Niederman <max@maxniederman.com>
2024-04-19 22:05:35 -0700

    feat: render landing page

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,11 @@ import Data.Maybe (fromJust)
 
 main :: IO ()
 main = hakyll $ do
-    match "content/*" $ do
+    match "content/*.md" $ do
         route   $ metadataRoute $ \meta ->
             constRoute $ fromJust (lookupString "slug" meta) <> ".html"
         compile $ pandocCompiler
+    
+    create ["content/index.html"] $ do
+        route $ constRoute "index.html"
+        compile copyFileCompiler
