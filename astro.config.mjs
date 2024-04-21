fe126cc3877be4f5907ae3e525eccd0f10850f05
Max Niederman <max@maxniederman.com>
2024-04-20 21:52:27 -0700

    reset Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,4 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import remarkGfm from "remark-gfm";
-import remarkGemoji from "remark-gemoji";
-import remarkSlug from "remark-slug";
-import remarkMath from "remark-math";
-import remarkSmartypants from "remark-smartypants";
-import remarkFootnotes from "remark-footnotes";
-import rehypeKatex from "rehype-katex";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: process.env.SITE ?? "https://maxniederman.com",
-  markdown: {
-    remarkPlugins: [
-      remarkGfm,
-      remarkGemoji,
-      remarkSlug,
-      remarkMath,
-      remarkSmartypants,
-      remarkFootnotes,
-    ],
-    rehypePlugins: [
-      [
-        rehypeKatex,
-        {
-          output: "html",
-          trust: true,
-        },
-      ],
-    ],
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "one-dark-pro",
-    },
-  },
-  integrations: [mdx()],
-});
+export default defineConfig({});
