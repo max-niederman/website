2f49c4e54d85cc56bc72b1d6d38e3bd3fdea0a83
Max Niederman <max@maxniederman.com>
2024-07-20 23:16:44 -0700

    implement clifford attractor rendering on index page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,12 @@
 import { z, defineCollection } from "astro:content";
 
 const postsCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+	}),
 });
 
 export const collections = {
-  posts: postsCollection,
+	posts: postsCollection,
 };
