0ab5946f51931f6f1e0771b8d66c1db06572562f
Max Niederman <max@maxniederman.com>
2024-07-24 20:58:17 -0700

    add form to control clifford explorer

--- src/components/clifford-attractor/Explorer.svelte
+++ src/components/clifford-attractor/Explorer.svelte
@@ -7,21 +7,67 @@
 		c: 1.0,
 		d: 0.7,
 	};
+	let numPoints = 1_000_000;
+	let attractorKey = {};
+
+	$: attractorKey = numPoints;
 </script>
 
 <form action="none">
-	<input type="range" bind:value={dynamics.a} min={-2} max={2} step={0.01} />
+	<math display="inline">
+		<mi>a</mi>
+		<mo>=</mo>
+		<mn>{dynamics.a}</mn>
+	</math>
+	<input type="range" bind:value={dynamics.a} min={-3} max={3} step={0.01} />
+	<math display="inline">
+		<mi>b</mi>
+		<mo>=</mo>
+		<mn>{dynamics.b}</mn>
+	</math>
+	<input type="range" bind:value={dynamics.b} min={-3} max={3} step={0.01} />
+	<math display="inline">
+		<mi>c</mi>
+		<mo>=</mo>
+		<mn>{dynamics.c}</mn>
+	</math>
+	<input type="range" bind:value={dynamics.c} min={-3} max={3} step={0.01} />
+	<math display="inline">
+		<mi>d</mi>
+		<mo>=</mo>
+		<mn>{dynamics.d}</mn>
+	</math>
+	<input type="range" bind:value={dynamics.d} min={-3} max={3} step={0.01} />
+
+	Number of points: {numPoints.toLocaleString()}
+	<input
+		type="range"
+		bind:value={numPoints}
+		min={0}
+		max={10_000_000}
+		step={100_000}
+	/>
+	Reset the attractor if it has become too noisy.
+	<button type="button" on:click={() => (attractorKey = {})}>Reset</button>
 </form>
 
 <div class="display-container">
-	<Attractor
-		dynamics={() => dynamics}
-		display={() => ({ scale: [0.5, 0.5], offset: [0, 0] })}
-	/>
+	{#key attractorKey}
+		<Attractor
+			dynamics={() => dynamics}
+			display={() => ({ scale: [0.25, 0.25], offset: [0, 0] })}
+			{numPoints}
+		/>
+	{/key}
 </div>
 
 <style lang="scss">
 	.display-container {
 		aspect-ratio: 1.618;
+	}
+
+	input,
+	button {
+		display: block;
 	}
 </style>
