51fbcf28219d014381cc16ee0fc12c6f8e990232
Max Niederman <max@maxniederman.com>
2024-08-14 22:18:10 -0700

    refactor(clifford): implement proper Vec2 class

---
+++ src/utils/math.ts
@@ -0,0 +1,41 @@
+export class Vec2 {
+    readonly #x: number;
+    readonly #y: number;
+
+    constructor(x: number = 0, y: number = x) {
+        this.#x = x;
+        this.#y = y;
+    }
+
+    get x(): number {
+        return this.#x;
+    }
+
+    get y(): number {
+        return this.#y;
+    }
+
+    add(other: Vec2): Vec2 {
+        return new Vec2(this.#x + other.#x, this.#y + other.#y);
+    }
+
+    sub(other: Vec2): Vec2 {
+        return new Vec2(this.#x - other.#x, this.#y - other.#y);
+    }
+
+    mul(scalar: number): Vec2 {
+        return new Vec2(this.#x * scalar, this.#y * scalar);
+    }
+
+    div(scalar: number): Vec2 {
+        return new Vec2(this.#x / scalar, this.#y / scalar);
+    }
+
+    dot(other: Vec2): number {
+        return this.#x * other.#x + this.#y * other.#y;
+    }
+
+    hademardProd(other: Vec2): Vec2 {
+        return new Vec2(this.#x * other.#x, this.#y * other.#y);
+    }
+}
