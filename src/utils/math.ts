d25182e4afb2815a8623d150e9c9ea9e59481051
Max Niederman <max@maxniederman.com>
2024-08-19 16:21:15 -0700

    feat: implement orbiting rings animation

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,49 +1,71 @@
 export class Vec2 {
-    readonly #x: number;
-    readonly #y: number;
+	readonly #x: number;
+	readonly #y: number;
 
-    constructor(x: number = 0, y: number = x) {
-        this.#x = x;
-        this.#y = y;
-    }
+	constructor(x: number = 0, y: number = x) {
+		this.#x = x;
+		this.#y = y;
+	}
 
-    get x(): number {
-        return this.#x;
-    }
+	get x(): number {
+		return this.#x;
+	}
 
-    get y(): number {
-        return this.#y;
-    }
+	get y(): number {
+		return this.#y;
+	}
 
-    add(other: Vec2): Vec2 {
-        return new Vec2(this.#x + other.#x, this.#y + other.#y);
-    }
+	add(other: Vec2): Vec2 {
+		return new Vec2(this.#x + other.#x, this.#y + other.#y);
+	}
 
-    sub(other: Vec2): Vec2 {
-        return new Vec2(this.#x - other.#x, this.#y - other.#y);
-    }
+	sub(other: Vec2): Vec2 {
+		return new Vec2(this.#x - other.#x, this.#y - other.#y);
+	}
 
-    mul(scalar: number): Vec2 {
-        return new Vec2(this.#x * scalar, this.#y * scalar);
-    }
+	mul(scalar: number): Vec2 {
+		return new Vec2(this.#x * scalar, this.#y * scalar);
+	}
 
-    div(scalar: number): Vec2 {
-        return new Vec2(this.#x / scalar, this.#y / scalar);
-    }
+	div(scalar: number): Vec2 {
+		return new Vec2(this.#x / scalar, this.#y / scalar);
+	}
 
-    dot(other: Vec2): number {
-        return this.#x * other.#x + this.#y * other.#y;
-    }
+	dot(other: Vec2): number {
+		return this.#x * other.#x + this.#y * other.#y;
+	}
 
-    hademardProd(other: Vec2): Vec2 {
-        return new Vec2(this.#x * other.#x, this.#y * other.#y);
-    }
+	hademardProd(other: Vec2): Vec2 {
+		return new Vec2(this.#x * other.#x, this.#y * other.#y);
+	}
 
-    hademardDiv(other: Vec2): Vec2 {
-        return new Vec2(this.#x / other.#x, this.#y / other.#y);
-    }
+	hademardDiv(other: Vec2): Vec2 {
+		return new Vec2(this.#x / other.#x, this.#y / other.#y);
+	}
 
-    componentwise(func: (x: number) => number): Vec2 {
-        return new Vec2(func(this.#x), func(this.#y));
-    }
+	componentwise(func: (x: number) => number): Vec2 {
+		return new Vec2(func(this.#x), func(this.#y));
+	}
+
+	l1Norm(): number {
+		return Math.abs(this.#x) + Math.abs(this.#y);
+	}
+
+	l2Norm(): number {
+		return Math.sqrt(this.#x ** 2 + this.#y ** 2);
+	}
+
+	lInfNorm(): number {
+		return Math.max(Math.abs(this.#x), Math.abs(this.#y));
+	}
 }
+
+export function randInterval(
+	min: number,
+	max: number,
+	rng: () => number = Math.random
+): number {
+	return min + rng() * (max - min);
+}
+
+export const TAU = 2 * Math.PI;
