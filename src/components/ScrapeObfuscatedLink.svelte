50a3d1f4e3ae86a1fd41c9c88476a36a1f0afd13
Max Niederman <max@maxniederman.com>
2024-08-20 23:44:48 -0700

    feat(index): add social links

---
+++ src/components/ScrapeObfuscatedLink.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import type { HTMLAttributeAnchorTarget } from "svelte/elements";
+
+	export let hrefBase64: string;
+	export let target: HTMLAttributeAnchorTarget = "_self";
+
+	let href: string = "";
+
+    onMount(() => {
+        href = atob(hrefBase64);
+    });
+</script>
+
+<a href={href} target={target}>
+	<slot />
+</a>
