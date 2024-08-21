9f208966a35b22dd2c44ec39116eea4598479f0a
Max Niederman <max@maxniederman.com>
2024-08-20 23:44:48 -0700

    feat(index): add social links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), mdx()]
+  integrations: [svelte(), mdx(), icon()]
 });
