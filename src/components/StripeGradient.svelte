3f8061689e9aabb1bfb7ff4f3d636047b5ab2cd7
Max Niederman <max@maxniederman.com>
2024-10-06 23:52:51 -0700

    feat(index): update stripe gradient colors

--- src/components/StripeGradient.svelte
+++ src/components/StripeGradient.svelte
@@ -13,12 +13,20 @@
 <canvas bind:this={canvas} />
 
 <style lang="scss">
+	@use "sass:color";
+
+	@function hex($color) {
+		@return "#" + str-slice(color.ie-hex-str($color), 4);
+	}
+
 	canvas {
 		width: 100%;
 		height: 100%;
 
-		--gradient-color-1: #449ce4;
-		--gradient-color-4: #4c57f6;
+		--gradient-color-1: #{hex(oklch(30% 0.15 -15))};
+		--gradient-color-2: #{hex(oklch(40% 0.15 15))};
+		--gradient-color-3: #{hex(oklch(50% 0.15 45))};
+		--gradient-color-4: #{hex(oklch(50% 0.15 75))};
 
 		background-color: var(--gradient-color-1);
 	}
