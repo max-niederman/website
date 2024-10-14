cd5c48a62026e5c2341fe98661488eb04120002d
Max Niederman <max@maxniederman.com>
2024-10-14 16:19:06 -0700

    feat: add some old posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), mdx(), icon()]
+  integrations: [svelte(), mdx(), icon()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }
 });
