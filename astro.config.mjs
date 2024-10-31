496eb341202123d93b9b4cd6920d5c5f5d8282e2
Max Niederman <max@maxniederman.com>
2024-10-31 16:01:22 -0700

    feat(posts): add reading time to metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,13 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import { remarkReadingTime } from "./src/markdown/reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [svelte(), mdx(), icon()],
 	markdown: {
-		remarkPlugins: [remarkMath],
+		remarkPlugins: [remarkMath, remarkReadingTime],
 		rehypePlugins: [rehypeKatex],
 		shikiConfig: {
 			theme: "catppuccin-macchiato",
