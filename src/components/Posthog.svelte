a0f557dfd8c5a8b6418adf82a317c802fbfd62f6
Max Niederman <max@maxniederman.com>
2025-06-09 19:55:58 -0700

    feat: add posthog snippet

---
+++ src/components/Posthog.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import posthog from "posthog-js";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		posthog.init("phc_t6xmW3mX5ce4NWuZ5vJCjkugMh4voIWD2aUvNgNbEZp", {
+			api_host: "https://us.i.posthog.com",
+			person_profiles: "identified_only",
+		});
+	});
+</script>
